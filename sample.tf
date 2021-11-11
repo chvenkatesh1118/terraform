79adf6b84a2ddf6058e94ef7779b681ca35412d3
Chanti <chvenkatesh.1118@gmail.com>
2021-11-11 17:02:50 +0530

    11-10

--- sample.tf
+++ sample.tf
@@ -1,36 +1,36 @@
-output "abc" {
-  value = 200
-}
-output "number" {
-  value = 200
-}
-output "boolean" {
-  value = true
-}
-variable "chanti" {
-  default = "checking string"
-}
-output "showstring" {
-  value = var.chanti
-
-}
-variable "num" {
-  default = 7013797108
-}
-
-output "mynumber" {
-  value = "mynumber = ${var.num}"
-}
-
-variable "chanti1" {
-  default = {
-    x = 25,
-    y =22,
-    z = "chanti2",
-    s = true,
-  }
-}
-
-output "details" {
-  value = "age = ${var.chanti1["x"]}"
-}
+#output "abc" {
+#  value = 200
+#}
+#output "number" {
+#  value = 200
+#}
+#output "boolean" {
+#  value = true
+#}
+#variable "chanti" {
+#  default = "checking string"
+#}
+#output "showstring" {
+#  value = var.chanti
+#
+#}
+#variable "num" {
+#  default = 7013797108
+#}
+#
+#output "mynumber" {
+#  value = "mynumber = ${var.num}"
+#}
+#
+#variable "chanti1" {
+#  default = {
+#    x = 25,
+#    y =22,
+#    z = "chanti2",
+#    s = true,
+#  }
+#}
+#
+#output "details" {
+#  value = "age = ${var.chanti1["x"]}"
+#}
