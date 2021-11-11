a46b1d721310a4b6b014164e4493140b7dd3ae22
Chanti <chvenkatesh.1118@gmail.com>
2021-11-11 16:50:46 +0530

    11-10

--- creat.tf
+++ creat.tf
@@ -41,12 +41,12 @@ resource "aws_security_group" "allow_sample"  {
   }
 }
 
-resource "aws_instance" "frontend" {
-  ami           = "ami-077fb3e62ddf0fa9a"
-  instance_type = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.allow_sample.id]
-
-  tags = {
-    Name = "frontend"
-  }
-}
+#resource "aws_instance" "frontend" {
+#  ami           = "ami-077fb3e62ddf0fa9a"
+#  instance_type = "t2.micro"
+#  vpc_security_group_ids = [aws_security_group.sg-0a45fd04183d267bd.id]
+#
+#  tags = {
+#    Name = "frontend"
+#  }
+#}
