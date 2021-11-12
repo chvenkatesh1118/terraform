826130f7afdc395c079bc0f0368045465a8aa2b1
Chanti <chvenkatesh.1118@gmail.com>
2021-11-12 13:30:45 +0530

    12-11

--- example/main.tf
+++ example/main.tf
@@ -1,9 +1,8 @@
 provider "aws" {
   region = "us-east-1"
 }
-variable "name" {
-  type = "string"
-  value = "name"
+resource "aws" "name"  {
+  default = "name"
 }
 output "name" {
   value = "name"
