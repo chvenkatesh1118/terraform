bbbca0a44d0069c53e9aaf20c616f0d692f8d97a
Chanti <chvenkatesh.1118@gmail.com>
2021-11-12 09:29:11 +0530

    12-11

---
+++ example/instance/main.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region = "us-east-1"
+}
+resource "aws_instance"  "frontend"  {
+  ami           = "ami-077fb3e62ddf0fa9a"
+  instance_type = "t2.micro"
+  vpc_security_group_ids = ["sg-0a45fd04183d267bd"]
+
+  tags = {
+    Name = "frontend"
+  }
+}
--- example/sg/main.tf
+++ example/sg/main.tf
@@ -40,16 +40,3 @@ resource "aws_security_group" "s_group"  {
     Name = "s_group"
   }
 }
-output "SGID" {
-  value = aws_security_group.s_group.id
-}
-
-resource "aws_instance"  "frontend"  {
-  ami           = "ami-077fb3e62ddf0fa9a"
-  instance_type = "t2.micro"
-  vpc_security_group_ids = ["sg-0a45fd04183d267bd"]
-
-  tags = {
-    Name = "frontend"
-  }
-}
