127171a2f36f41dc776735d13d1bd0d4486e6ec6
chvenkatesh1118 <chvenkatesh.1118@gmail.com>
2021-11-22 12:23:38 +0530

    22-11

--- jenkins.sh
+++ jenkins.sh
@@ -2,11 +2,32 @@
  wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo --no-check-certificate
  rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
 
-  yum install epel-release
-  yum install java-11-openjdk-devel -y
-  yum install jenkins -y
+  yum install epel-release >> /dev/log1
+   if [&1 -eq 0] then 
+    echo epel instald
+   else
+    echo epel not installed  
+  yum install java-11-openjdk-devel -y  >> /dev/log1
+   if [&1 -eq 0] then 
+    echo java instald
+   else
+    echo java not installed
+
+  yum install jenkins -y >> /dev/log1
+   if [&1 -eq 0] then 
+    echo jenkins instald
+   else
+    echo jenkins not installed
 
   systemctl enable jenkins
+   if [&1 -eq 0] then 
+    echo jenkins enabled
+   else
+    echo jenkins enabled not
   systemctl start jenkins
+   if [&1 -eq 0] then 
+    echo jenkins started
+   else
+    echo jenkins started not
   cd /var/log/jenkins
   cat jenkins.log
