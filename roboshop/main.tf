21b39c7e90a5cc6ed2464a7fefad978a9a858cb3
Chanti <chvenkatesh.1118@gmail.com>
2021-11-26 07:50:37 +0530

    26-11

--- roboshop/main.tf
+++ roboshop/main.tf
@@ -13,6 +13,7 @@ resource "aws_spot_instance_request" "cheap_worker" {
   }
 }
 resource "aws_ec2_tag" "tags" {
+  count = length(var.components)
   key         = "name"
   resource_id = element(aws_spot_instance_request.cheap_worker.*.spot_instance_id, count.index)
   value       = element(var.components, count.index)
