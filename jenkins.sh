214e2c8d74e6d76a35971b49b3ec3da90104d8ab
Chanti <chvenkatesh.1118@gmail.com>
2022-01-27 08:06:17 +0530

    26-11

--- jenkins.sh
+++ jenkins.sh
@@ -1,26 +1,35 @@
 #!/bin/bash
- wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo --no-check-certificate >> /dev/log1
-   echo jenkins downloaded
- rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key >> /dev/log1
-   echo key downloaded
-
-  yum install epel-release >> /dev/log1
-   echo epel installed
-
-  yum install java-11-openjdk-devel -y  >> /dev/log1
-   echo java installed
-
-
-  yum install jenkins -y >> /dev/log1
-   echo jenkins installed
-
-
-  systemctl enable jenkins
-  echo jenkins enabled
-
-  systemctl start jenkins
-
-    echo jenkins started
-     cat /var/lib/jenkins/secrets/initialAdminPassword
-
-
+# wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo --no-check-certificate >> /dev/log1
+#   echo jenkins downloaded
+# rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key >> /dev/log1
+#   echo key downloaded
+#
+#  yum install epel-release >> /dev/log1
+#   echo epel installed
+#
+#  yum install java-11-openjdk-devel -y  >> /dev/log1
+#   echo java installed
+#
+#
+#  yum install jenkins -y >> /dev/log1
+#   echo jenkins installed
+#
+#
+#  systemctl enable jenkins
+#  echo jenkins enabled
+#
+#  systemctl start jenkins
+#
+#    echo jenkins started
+#     cat /var/lib/jenkins/secrets/initialAdminPassword
+#
+#
+sudo wget -O /etc/yum.repos.d/jenkins.repo \
+    https://pkg.jenkins.io/redhat-stable/jenkins.repo
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
+sudo yum upgrade
+sudo yum install epel-release java-11-openjdk-devel
+sudo yum install jenkins
+sudo systemctl daemon-reload
+sudo systemctl start jenkins
+sudo systemctl status jenkins
