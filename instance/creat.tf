ffc31f338602a6b71b8a9c58e31893bf7e191d91
Chanti <chvenkatesh.1118@gmail.com>
2022-02-16 19:06:01 +0530

    11-02

--- instance/creat.tf
+++ instance/creat.tf
@@ -1,19 +1,22 @@
-provider "aws" {
-  region = "us-east-1"
-}
+#provider "aws" {
+#  region = "us-east-1"
+#}
+#
+#
+#resource "aws_instance" "frontend" {
+#   ami           = "ami-0d997c5f64a74852c"
+#   instance_type  = "t2.micro"
+#
+#    subnet_id   =  "subnet-0f42b250c3cf1d75c"
+#
+#   tags = {
+#      Name = "frontend"
+#    }
+#  }
 
 
-resource "aws_instance" "frontend" {
-   ami           = "ami-0d997c5f64a74852c"
-   instance_type  = "t2.micro"
 
-    subnet_id   =  "subnet-0f42b250c3cf1d75c"
-
-   tags = {
-      Name = "frontend"
-    }
-  }
-
+touch chanti
 
 
 
