c24cd5a9ce69c6eab184586815057ef8bd8a9f4c
Chanti <chvenkatesh.1118@gmail.com>
2022-02-16 18:25:43 +0530

    11-02

--- instance/creat.tf
+++ instance/creat.tf
@@ -19,7 +19,7 @@ resource "aws_security_group" "newsg" {
     description      = "TLS from VPC"
     from_port        = 22
     to_port          = 80
-    protocol         = "ssh"
+    protocol         = "tcp"
      cidr_blocks      = ["0.0.0.0/0"]
 
 
