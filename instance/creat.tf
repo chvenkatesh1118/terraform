95a4d84d4080f0ba4d43a48db6e8546d67d89f95
Chanti <chvenkatesh.1118@gmail.com>
2022-02-16 18:40:55 +0530

    11-02

--- instance/creat.tf
+++ instance/creat.tf
@@ -15,31 +15,5 @@ resource "aws_instance" "frontend" {
   }
 
 
-resource "aws_security_group" "newsg" {
-  name        = "newsg"
-  description = "Allow TLS inbound traffic"
-  vpc_id      = "vpc-0bf57a4f41c708816"
 
-  ingress {
-    description      = "TLS from VPC"
-    from_port        = 22
-    to_port          = 80
-    protocol         = "tcp"
-     cidr_blocks      = ["0.0.0.0/0"]
-
-
-  }
-
-  egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-
-  }
-
-  tags = {
-    Name = "newsg"
-  }
-}
 
