d75237fb1fe8665eacacdb74bda1ebdd41aa9d7e
Chanti <chvenkatesh.1118@gmail.com>
2022-02-16 16:04:36 +0530

    11-02

---
+++ intsance/creat.tf
@@ -0,0 +1,10 @@
+resource "aws_instance" "var.NAME" {
+   ami           = "ami-0d997c5f64a74852c"
+   instance_type = "t2.micro"
+   vpc_id            = "vpc-0bf57a4f41c708816"
+    subnet_id   = "subnet-0f42b250c3cf1d75c"
+    vpc_security_group_ids = "sgr-07d38fac622e24c40"
+   tags = {
+      Name = var.NAME
+    }
+  }
---
+++ intsance/var.tf
@@ -0,0 +1,1 @@
+NAME= {frontend,db}
