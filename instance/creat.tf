e9270acdfd27f5a18d9732470c3b4105846d9a5f
Chanti <chvenkatesh.1118@gmail.com>
2022-02-16 16:49:19 +0530

    11-02

--- instance/creat.tf
+++ instance/creat.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "frontend" {
    ami           = "ami-0d997c5f64a74852c"
    instance_type  = "t2.micro"
-    vpc_id            = "vpc-0bf57a4f41c708816"
+
     subnet_id   =  "subnet-0f42b250c3cf1d75c"
     vpc_security_group_ids = aws_security_group.newsg.id
    tags = {
