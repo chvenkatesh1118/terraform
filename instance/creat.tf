d644614a811c3c91ee85d9bcabfa44b68d2aeb13
Chanti <chvenkatesh.1118@gmail.com>
2022-02-16 16:30:34 +0530

    11-02

--- instance/creat.tf
+++ instance/creat.tf
@@ -3,8 +3,37 @@ resource "aws_instance" "instance" {
    instance_type = "t2.micro"
    vpc_id            = "vpc-0bf57a4f41c708816"
     subnet_id   = "subnet-0f42b250c3cf1d75c"
-    vpc_security_group_ids = "sgr-07d38fac622e24c40"
+    vpc_security_group_ids = aws_security_group.newsg.id
    tags = {
       Name = frontend
     }
   }
+
+
+resource "aws_security_group" "newsg" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description      = "TLS from VPC"
+    from_port        = 22
+    to_port          = 80
+    protocol         = "ssh"
+     cidr_blocks      = ["0.0.0.0/0"]
+
+
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+
+  }
+
+  tags = {
+    Name = "newsg"
+  }
+}
