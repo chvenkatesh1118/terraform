61612c5b15f4db17a2cf712479fdc350d54d7179
Chanti <chvenkatesh.1118@gmail.com>
2022-02-21 16:08:10 +0530

    11-02

--- instance/creat.tf
+++ instance/creat.tf
@@ -8,7 +8,7 @@ resource "aws_instance" "instance" {
    instance_type  = "t2.micro"
    subnet_id   =  "subnet-0f42b250c3cf1d75c"
 
-   count = var.COMPONENTS.index
+  count         = length(var.COMPONENTS)
    tags = {
       Name = var.COMPONENTS
     }
