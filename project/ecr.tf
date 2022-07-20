aa4aba9d9f1f1638c4810ecbe4cabd50cc0b1557
Chanti <chvenkatesh.1118@gmail.com>
2022-07-20 13:13:45 +0530

    24-03

---
+++ project/ecr.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+  alias  = "us_east_1"
+  region = "us-east-1"
+}
+
+resource "aws_ecr_repository" "ecr" {
+  name                 = "chantiecr"
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
