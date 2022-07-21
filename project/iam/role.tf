16b74bea913e3d959b5d37c3358e55d54966f574
Chanti <chvenkatesh.1118@gmail.com>
2022-07-21 07:05:25 +0530

    24-03

--- project/iam/role.tf
+++ project/iam/role.tf
@@ -1,20 +1,20 @@
-resource "aws_iam_role" "ecsrole" {
-  name = "ecsrole"
-
-  assume_role_policy = jsonencode({
-   "Version" : "2012-10-17",
-    "Statement" : [
-      {
-        "Sid" : "VisualEditor0",
-        "Effect" : "Allow",
-        "Action" : "ecs:*",
-        "Resource" : "*"
-      }
-    ],
-  }
-  )
-
-  tags = {
-    tag-key = "tag-value"
-  }
-}
+#resource "aws_iam_role" "ecsrole" {
+#  name = "ecsrole"
+#
+#  assume_role_policy = jsonencode({
+#   "Version" : "2012-10-17",
+#    "Statement" : [
+#      {
+#        "Sid" : "VisualEditor0",
+#        "Effect" : "Allow",
+#        "Action" : "ecs:*",
+#        "Resource" : "*"
+#      }
+#    ],
+#  }
+#  )
+#
+#  tags = {
+#    tag-key = "tag-value"
+#  }
+#}
