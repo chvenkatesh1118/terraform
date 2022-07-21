f6fcaf370e979c231e918ecb77af661540adb522
Chanti <chvenkatesh.1118@gmail.com>
2022-07-21 07:29:55 +0530

    24-03

--- project/iam/role.tf
+++ project/iam/role.tf
@@ -1,20 +1,44 @@
-#resource "aws_iam_role" "ecsrole" {
-#  name = "ecsrole"
-#
-#  assume_role_policy = jsonencode({
-#   "Version" : "2012-10-17",
-#    "Statement" : [
-#      {
-#        "Sid" : "VisualEditor0",
-#        "Effect" : "Allow",
-#        "Action" : "ecs:*",
-#        "Resource" : "*"
-#      }
-#    ],
-#  }
-#  )
-#
-#  tags = {
-#    tag-key = "tag-value"
-#  }
-#}
+resource "aws_iam_role" "role" {
+  name = "ecs-role"
+
+  assume_role_policy = <<EOF
+{
+ Version": "2008-10-17",
+    "Statement": [
+        {
+            "Sid": "",
+            "Effect": "Allow",
+            "Principal": {
+                "Service": "ecs.amazonaws.com"
+            },
+            "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_policy" "policy" {
+  name        = "ecs_policy"
+  path        = "/"
+  description = "My test policy"
+
+  # Terraform's "jsonencode" function converts a
+  # Terraform expression result to valid JSON syntax.
+  policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Sid" : "VisualEditor0",
+        "Effect" : "Allow",
+        "Action" : "ecs:*",
+        "Resource" : "*"
+      },
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "test-attach" {
+  role       = aws_iam_role.role.name
+  policy_arn = aws_iam_policy.policy.arn
+}
