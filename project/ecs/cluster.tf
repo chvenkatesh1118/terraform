bd7109ca5e90212926b080f9fbfeb16abcacdb1e
Chanti <chvenkatesh.1118@gmail.com>
2022-07-22 07:19:44 +0530

    24-03

--- project/ecs/cluster.tf
+++ project/ecs/cluster.tf
@@ -18,3 +18,30 @@ resource "aws_ecs_cluster_capacity_providers" "providers" {
     capacity_provider = "EC2"
   }
 }
+
+
+resource "aws_ecs_capacity_provider" "EC2" {
+  name = "EC2"
+
+  auto_scaling_group_provider {
+    auto_scaling_group_arn         = aws_autoscaling_group.autoscale.arn
+    managed_termination_protection = "ENABLED"
+
+    managed_scaling {
+      maximum_scaling_step_size = 1
+      minimum_scaling_step_size = 1
+      status                    = "ENABLED"
+      target_capacity           = 1
+    }
+  }
+}
+
+
+resource "aws_autoscaling_group" "autoscale" {
+
+  tag {
+    key                 = "AmazonECSManaged"
+    value               = true
+    propagate_at_launch = true
+  }
+}
