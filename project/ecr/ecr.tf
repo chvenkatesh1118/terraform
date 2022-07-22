485377db9c58775095f8d0178be9178f78f11c2e
Chanti <chvenkatesh.1118@gmail.com>
2022-07-22 06:57:58 +0530

    24-03

--- project/ecr/ecr.tf
+++ project/ecr/ecr.tf
@@ -1,12 +1,12 @@
-provider "aws" {
-  region = "us-east-1"
-}
-
-resource "aws_ecr_repository" "ecr" {
-  name                 = "chantiecr"
-  image_tag_mutability = "MUTABLE"
-
-  image_scanning_configuration {
-    scan_on_push = true
-  }
-}
+#provider "aws" {
+#  region = "us-east-1"
+#}
+#
+#resource "aws_ecr_repository" "ecr" {
+#  name                 = "chantiecr"
+#  image_tag_mutability = "MUTABLE"
+#
+#  image_scanning_configuration {
+#    scan_on_push = true
+#  }
+#}
--- project/ecs/cluster.tf
+++ project/ecs/cluster.tf
@@ -4,5 +4,17 @@ resource "aws_ecs_cluster" "ecscluster" {
   setting {
     name  = "containerInsights"
     value = "enabled"
+
+  }
+}
+resource "aws_ecs_cluster_capacity_providers" "providers" {
+  cluster_name = aws_ecs_cluster.ecscluster.name
+
+  capacity_providers = ["EC2"]
+
+  default_capacity_provider_strategy {
+    base              = 1
+    weight            = 1
+    capacity_provider = "EC2"
   }
 }
