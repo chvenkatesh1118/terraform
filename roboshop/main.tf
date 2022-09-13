3913377a40ee237903cd32cbd88b07608564144e
Chanti <chvenkatesh.1118@gmail.com>
2022-09-13 20:34:21 +0530

    13-09

--- roboshop/main.tf
+++ roboshop/main.tf
@@ -1,55 +1,14 @@
-#provider "aws" {
-#  region = "us-east-1"
-#}
-##
+provider "aws" {
+  region = "us-east-1"
+}
 
-##resource "aws_instance" "instance" {
-##  ami           = "ami-0eb5f3f64b10d3e0e"
-##  count = length(var.components)
-##  instance_type = "t2.micro"
-##  vpc_security_group_ids = ["sg-0a45fd04183d267bd"]
-##
-##  tags = {
-##    Name = element(var.components,count.index )
-##  }
-##}
-##resource "aws_ec2_tag" "tags" {
-##  count = length(var.components)
-##  key         = "Name"
-##  resource_id = element(aws_instance.instance.*.spot_instance_id, count.index)
-##  value       = element(var.components, count.index)
-##
-##}
-##
-##
-##data "aws_ami" "ami" {
-##  most_recent = true
-##  name_regex  = "^Cent*"
-##  owners      = ["973714476881"]
-##}
-##
-## variable "components" {
-##   default = ["Dockerfile", "mongodb" ]
-##
-## }
-#
-#
-#resource "aws_instance" "mahi" {
-#  ami = "ami-0855cab4944392d0a"
-#  instance_type = "t3.micro"
-#  tags = {
-#    Name = "mahi
-#  }
-#}
-#output "public_ip" {
-#  value = "aws_instance.mahi.public_ip"
-#
-#
-#}
-#
-#resource "aws_ec2_tag" "tags" {
-#  count = "1"
-#  resource_id = "aws_instance.mahi.*.private_ip"
-#  key         = "Name"
-#  value       = "1"
-#}
+resource "aws_instance" "server2" {
+  ami = "i-04e1e0c5b6aa444da"
+  subnet_id = "subnet-003e1119782e07374"
+  security_groups = ["sg-07f5cb83e6215f65c"]
+  count = "1"
+  instance_type = "t2.micro"
+}
+tags = {
+  Name = "server2"
+}
