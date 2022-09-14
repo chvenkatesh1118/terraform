7f1fcac4ebbdcf7653b31226f5b396947008d299
Chanti <chvenkatesh.1118@gmail.com>
2022-09-14 21:34:16 +0530

    13-09

--- roboshop/main.tf
+++ roboshop/main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "server2" {
-  ami             = "i-04e1e0c5b6aa444da"
+  ami             = "ami-0bb6af715826253bf"
   subnet_id       = "subnet-003e1119782e07374"
   security_groups = ["sg-07f5cb83e6215f65c"]
   count           = "1"
