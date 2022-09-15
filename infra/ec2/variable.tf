35d2039b11486139b43f96988c45484eed114666
Chanti <chvenkatesh.1118@gmail.com>
2022-09-15 08:13:26 +0530

    13-09

--- infra/ec2/variable.tf
+++ infra/ec2/variable.tf
@@ -1,9 +1,9 @@
 variable "ec2_information" {
 
-    name    = ec2-3
-    ami = ami-0bb6af715826253bf
-    subnet = subnet-003e1119782e07374
-    count = 1
+    name    = "ec2-3"
+    ami = "ami-0bb6af715826253bf"
+    subnet = "subnet-003e1119782e07374"
+    count = "1"
 
 
 }
