afcf082a94fb26ed7fcea2a806888a4809fc9ba5
Chanti <chvenkatesh.1118@gmail.com>
2022-09-17 17:24:19 +0530

    13-09

--- infra/ec2/main.tf
+++ infra/ec2/main.tf
@@ -1,9 +1,3 @@
-variable "name" {}
-variable "type" {}
-variable "ami" {}
-variable "subnet" {}
-
-
 
 provider "aws" {
   region = "us-east-1"
@@ -13,16 +7,23 @@ resource "aws_instance" "server2" {
   ami             = var.ami
   subnet_id       = var.subnet
   security_groups = ["sg-07f5cb83e6215f65c"]
-  count           = "1"
+#  count         = "1"
   instance_type   = var.type
   tags = {
     Name = var.name
   }
 }
-output "privateip" {
+
+
+output  "private_ip" {
   value = aws_instance.server2.private_ip
 }
 
-output "publicip" {
+output "public_ip" {
   value = aws_instance.server2.public_ip
 }
+
+variable "name" {}
+variable "type" {}
+variable "ami" {}
+variable "subnet" {}
