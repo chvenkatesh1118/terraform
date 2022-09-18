59c3fdc74747c5a0e8e361752c5c5e473681c6c6
Chanti <chvenkatesh.1118@gmail.com>
2022-09-18 08:29:59 +0530

    13-09

--- infra/s3/main.tf
+++ infra/s3/main.tf
@@ -1,68 +1,15 @@
-resource "aws_s3_bucket" "bucket" {
-  bucket = "my-bucket"
-  acl    = "private"
+resource "aws_s3_bucket" "s3bucket" {
+  bucket = "chanti"
+  storage_class = "STANDARD_IA"
 
-  lifecycle_rule {
-    id      = "log"
-    enabled = true
+  tags = {
+    Name        = "chanti"
 
-    prefix = "log/"
-
-    tags = {
-      rule      = "log"
-      autoclean = "true"
-    }
-
-    transition {
-      days          = 30
-      storage_class = "STANDARD_IA" # or "ONEZONE_IA"
-    }
-
-    transition {
-      days          = 60
-      storage_class = "GLACIER"
-    }
-
-    expiration {
-      days = 90
-    }
   }
 
-  lifecycle_rule {
-    id      = "tmp"
-    prefix  = "tmp/"
-    enabled = true
 
-    expiration {
-      date = "2016-01-12"
-    }
-  }
 }
-
-resource "aws_s3_bucket" "versioning_bucket" {
-  bucket = "my-versioning-bucket"
+resource "aws_s3_bucket_acl" "example" {
+  bucket = aws_s3_bucket.s3bucket.id
   acl    = "private"
-
-  versioning {
-    enabled = true
-  }
-
-  lifecycle_rule {
-    prefix  = "config/"
-    enabled = true
-
-    noncurrent_version_transition {
-      days          = 30
-      storage_class = "STANDARD_IA"
-    }
-
-    noncurrent_version_transition {
-      days          = 60
-      storage_class = "GLACIER"
-    }
-
-    noncurrent_version_expiration {
-      days = 90
-    }
-  }
 }
